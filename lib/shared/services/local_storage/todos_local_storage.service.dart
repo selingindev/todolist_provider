e9dc7f7763f2c05de638460297977bc84a97bc08
Arthur Selingin <arthur_santos@outlook.com.br>
2024-08-24 22:38:19 -0300

    coemcando service da todos

---
+++ lib/shared/services/local_storage/todos_local_storage.service.dart
@@ -0,0 +1,10 @@
+import 'package:todolist_provider/shared/interfaces/local_storage_interface.dart';
+
+class TodosLocalStorageService {
+  final ILocalStorageService _localStorageService;
+
+  TodosLocalStorageService(this._localStorageService);
+
+  
+
+}
