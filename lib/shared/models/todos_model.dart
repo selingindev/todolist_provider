d1d8d76ef7e3827d467914b90c8508ef02af473b
Arthur Selingin <arthur_santos@outlook.com.br>
2024-08-27 21:52:08 -0300

    Map no model

--- lib/shared/models/todos_model.dart
+++ lib/shared/models/todos_model.dart
@@ -9,4 +9,12 @@ class TodosModel {
   TodosModel({String? cId, required this.title, this.description, DateTime? cDate}) : id = cId ?? const Uuid().v4(),
     date = cDate ?? DateTime.now();
 
+  Map<String, dynamic> toMap(){
+    return{
+        'id': id,
+        'title': title,
+        'description': description,
+        'date' : date.microsecondsSinceEpoch,
+    };
+  }
 }
