10e01df73c369691bb3b7a54de1f5826018da54f
Arthur Selingin <arthur_santos@outlook.com.br>
2024-08-28 20:24:08 -0300

    implementando interface da todos

--- lib/shared/interfaces/todos_local_storage_interface.dart
+++ lib/shared/interfaces/todos_local_storage_interface.dart
@@ -1,5 +1,6 @@
 import 'package:todolist_provider/shared/models/todos_model.dart';
 
 abstract class ITodosLocalStorageService {
- Future<String?> setTodos(List<TodosModel> todos);
-}
+  Future<String?> setTodos(List<TodosModel> todos);
+  Future<(String? error, List<TodosModel>? todos)> getTodos(); 
+}   
