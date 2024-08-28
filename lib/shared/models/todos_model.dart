9dc3a68ec5453e5884099925bc887e0579ea2512
Arthur Selingin <arthur_santos@outlook.com.br>
2024-08-28 20:23:51 -0300

    atualizando todo model

--- lib/shared/models/todos_model.dart
+++ lib/shared/models/todos_model.dart
@@ -6,15 +6,27 @@ class TodosModel {
   final String? description;
   final DateTime date;
 
-  TodosModel({String? cId, required this.title, this.description, DateTime? cDate}) : id = cId ?? const Uuid().v4(),
-    date = cDate ?? DateTime.now();
+  TodosModel(
+      {String? cId, required this.title, this.description, DateTime? cDate})
+      : id = cId ?? const Uuid().v4(),
+        date = cDate ?? DateTime.now();
 
-  Map<String, dynamic> toMap(){
-    return{
-        'id': id,
-        'title': title,
-        'description': description,
-        'date' : date.microsecondsSinceEpoch,
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'date': date.microsecondsSinceEpoch,
     };
+  }
+
+  factory TodosModel.fromMap(Map<String, dynamic> map) {
+    return TodosModel(
+        cId: map['id'],
+        title: map['title'],
+        description: map['description'],
+        cDate: DateTime.fromMillisecondsSinceEpoch(
+          map['date'],
+        ));
   }
 }
