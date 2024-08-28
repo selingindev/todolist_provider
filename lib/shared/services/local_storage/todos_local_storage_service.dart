bf355ba0bcc8899e3ff909b48945283403983989
Arthur Selingin <arthur_santos@outlook.com.br>
2024-08-27 21:52:35 -0300

    setTodos

--- lib/shared/services/local_storage/todos_local_storage_service.dart
+++ lib/shared/services/local_storage/todos_local_storage_service.dart
@@ -1,16 +1,31 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:todolist_provider/shared/const/menssage_const.dart';
+import 'package:todolist_provider/shared/errors/local_storage_exception.dart';
 import 'package:todolist_provider/shared/interfaces/local_storage_interface.dart';
 import 'package:todolist_provider/shared/interfaces/todos_local_storage_interface.dart';
 import 'package:todolist_provider/shared/models/todos_model.dart';
 
-class TodosLocalStorageService implements ITodosLocalStorageService{
-  final ILocalStorageService _ILocalStorageService;
-  
-  TodosLocalStorageService(this._ILocalStorageService);
-  
-  
-  Future<String?> setTodos(List<TodosModel> todos) async{
+class TodosLocalStorageService implements ITodosLocalStorageService {
+  final todosKey = "todos";
 
-  };
-    
+  final ILocalStorageService _iLocalStorageService;
 
+  TodosLocalStorageService(this._iLocalStorageService);
+
+  @override
+  Future<String?> setTodos(List<TodosModel> todos) async {
+    try {
+      final String data = jsonEncode(
+        todos.map((todo) => todo.toMap()).toList(),
+      );
+      await _iLocalStorageService.set(todosKey, data);
+      return null;
+    } on LocalStorageException {
+      return "Erro ao Salvar Tarefas";
+    } catch (error, st) {
+      log("Error Saving todos", error: error, stackTrace: st);
+      return defaultErrorMenssage;
+    }
+  }
 }
