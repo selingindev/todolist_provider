d010d104780bebda5b7793e7ec0580fa433c738a
Arthur Selingin <arthur_santos@outlook.com.br>
2024-09-03 20:50:06 -0300

    começnado screen adicionar todo

---
+++ lib/features/add_todos/screens/add_todo_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:todolist_provider/shared/widgets/texts/text_widget.dart';
+
+class AddTodoScreen extends StatefulWidget {
+  const AddTodoScreen({super.key});
+
+  @override
+  State<AddTodoScreen> createState() => _AddTodoScreenState();
+}
+
+class _AddTodoScreenState extends State<AddTodoScreen> {
+    final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const TextWidget('Criar uma nova tarefa', ccolor: Colors.black, cfontSize: 20 ,),
+      ), 
+      body: SingleChildScrollView(
+        child: Padding(padding: EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              
+            ],
+          )),
+        ),
+      )
+    );
+  }
+}
