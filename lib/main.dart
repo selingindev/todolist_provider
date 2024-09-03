cffa84ee88041f424fed0a0733454410b76dd590
Arthur Selingin <arthur_santos@outlook.com.br>
2024-09-03 02:27:24 -0300

    motando o change notifier no meu main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
+import 'package:todolist_provider/features/todos/controllers/todo_controller.dart';
 import 'package:todolist_provider/features/todos/screens/todos_screens.dart';
+import 'package:todolist_provider/shared/services/local_storage/done_todos_local_storage_service.dart';
+import 'package:todolist_provider/shared/services/local_storage/local_storage_service.dart';
+import 'package:todolist_provider/shared/services/local_storage/todos_local_storage_service.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,21 +16,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter ToDo List',
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          foregroundColor: Color.fromARGB(255, 0, 0, 0),
-          backgroundColor: Colors.white
+    return ChangeNotifierProvider (
+      create: (_) => TodoController(
+         DoneTodosLocalStorageService(
+          LocalStorageService(),
+          ),
+        TodosLocalStorageService(
+        LocalStorageService(),),
+       ),
+      child: MaterialApp(
+        title: 'Flutter ToDo List',
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+            foregroundColor: Color.fromARGB(255, 0, 0, 0),
+            backgroundColor: Colors.white
+          ),
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 89, 47, 161),
+              surface: const Color.fromARGB(255, 29, 28, 28)),
+          useMaterial3: true,
         ),
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 89, 47, 161),
-            surface: const Color.fromARGB(255, 29, 28, 28)),
-        useMaterial3: true,
+        home: const TodosScreens(title: 'Lista de Tarefas - Arthur Selingin'),
+        localizationsDelegates: const [GlobalMaterialLocalizations.delegate, GlobalCupertinoLocalizations.delegate],
+        supportedLocales: const [Locale('pt', 'BR')],
       ),
-      home: const TodosScreens(title: 'Lista de Tarefas - Arthur Selingin'),
-      localizationsDelegates: const [GlobalMaterialLocalizations.delegate],
-      supportedLocales: const [Locale('pt', 'BR')],
     );
   }
 }
