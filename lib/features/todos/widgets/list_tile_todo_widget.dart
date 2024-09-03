5036fe9db046f1bf4b648b15865567f4ee47d8b1
Arthur Selingin <arthur_santos@outlook.com.br>
2024-09-03 14:50:14 -0300

    tirando todo model do widget

--- lib/features/todos/widgets/list_tile_todo_widget.dart
+++ lib/features/todos/widgets/list_tile_todo_widget.dart
@@ -1,37 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:todolist_provider/features/todos/widgets/todo_checkbox_widget.dart';
-import 'package:todolist_provider/shared/models/todos_model.dart';
 import 'package:todolist_provider/shared/widgets/texts/text_widget.dart';
 import 'todo_date_time_widget.dart';
 
-final List<TodosModel> todos = [
-  TodosModel(title: 'Sou uma tarefa complicada', description: 'voce consegue'),
-  TodosModel(title: 'ok', description: 'ok'),
-  TodosModel(title: 'Sou uma tarefa complicada', description: 'voce cnsegue'),
-  TodosModel(title: 'ok', description: 'ok'),
-  TodosModel(title: 'ok', description: 'ok'),
-  TodosModel(title: 'ok', description: 'ok'),
-  TodosModel(title: 'Sou uma tarefa complicada', description: 'voce cnsegue'),
-    TodosModel(title: 'ok', description: 'ok'),
-  TodosModel(title: 'Sou uma tarefa complicada', description: 'voce cnsegue'),
-    TodosModel(title: 'ok', description: 'ok'),
-  TodosModel(title: 'Sou uma tarefa complicada', description: 'voce cnsegue'),
-    TodosModel(title: 'ok', description: 'ok'),
-  TodosModel(title: 'Sou uma tarefa complicada', description: 'voce cnsegue'),
-    TodosModel(title: 'ok', description: 'ok'),
-  TodosModel(title: 'Sou uma tarefa complicada', description: 'voce cnsegue'),
-];
 
 class ListTileTodoWidget extends StatelessWidget {
-  const ListTileTodoWidget({super.key});
+  final dynamic todo;
+
+  const ListTileTodoWidget(this.todo, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: todos.length,
-      itemBuilder: (_, int index) {
-        final todo = todos[index];
-        return ListTile(
+    return ListTile(
           leading: TodoCheckboxWidget(todo),
           title: TextWidget(cfontSize:18,todo.title),
           subtitle: TextWidget(cfontSize: 14, todo.description ?? ''), 
@@ -41,7 +21,6 @@ class ListTileTodoWidget extends StatelessWidget {
           minVerticalPadding: 16,
 
         );
-      },
-    );
   }
 }
+
