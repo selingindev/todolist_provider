41f4f649d9f0f1f0733b0ddf761ce3b7bdc79b72
Arthur Selingin <arthur_santos@outlook.com.br>
2024-09-05 22:59:14 -0300

    adicionando dispose e salvando dados

--- lib/features/add_todos/screens/add_todo_screen.dart
+++ lib/features/add_todos/screens/add_todo_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todolist_provider/features/todos/controllers/todo_controller.dart';
+import 'package:todolist_provider/shared/models/todos_model.dart';
 import 'package:todolist_provider/shared/widgets/inputs/text_input_widget.dart';
 import 'package:todolist_provider/shared/widgets/texts/text_widget.dart';
 
@@ -11,6 +14,42 @@ class AddTodoScreen extends StatefulWidget {
 
 class _AddTodoScreenState extends State<AddTodoScreen> {
     final _formKey = GlobalKey<FormState>();
+    final _titleTEC = TextEditingController();
+    final _titleFN  = FocusNode();
+    
+    final _descTEC =  TextEditingController();
+    final _descFN  = FocusNode();
+    
+    final _todoDateTEC = TextEditingController();
+    final _todoDateFN  = FocusNode();
+    
+    late DateTime todoDate;
+
+    @override
+  void dispose() {
+      _titleTEC.dispose();
+      _titleFN.dispose();
+      _descTEC.dispose();
+      _descFN.dispose();
+      _todoDateTEC.dispose();
+      _todoDateFN.dispose();
+    
+        super.dispose();
+  }
+  Future<void> _addTodo(BuildContext context) async{
+    if(_formKey.currentState!.validate()){
+      final todosCtrl = Provider.of<TodoController>(context, listen:false);
+      final String? error = await todosCtrl.addTodos(TodosModel(title: _titleTEC.text, description: _descTEC.text , cDate: todoDate));
+      
+    if(context.mounted){
+      if(error != null){
+
+      }else{ 
+        Navigator.of(context).pop();
+      }
+    }
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
